422af03a520f2268ae6da2889c12f2d3b22d9177
Mark Ehresman <435089@gmail.com>
2021-02-08 18:56:47 -0600

    added changes

--- slack_python_logging/slack_logger.py
+++ slack_python_logging/slack_logger.py
@@ -29,14 +29,14 @@ def getLogger(
     slack_logger.addHandler(stream_handler)
 
     # Attempt to log to Slack, but don't if there isn't a webhook url
-    try:
-        webhook_url: str = os.environ["slack_webhook_url"]
-        slack_handler: logging.Handler = SlackHandler(webhook_url)
-        slack_handler.setLevel(slack_loglevel)
-        slack_formatter: logging.Formatter = SlackFormatter(app_name)
-        slack_handler.setFormatter(slack_formatter)
-        slack_logger.addHandler(slack_handler)
-    except KeyError:
-        slack_logger.warning("No Slack webhook -- not logging to slack")
+    #try:
+    #    webhook_url: str = os.environ["slack_webhook_url"]
+    #    slack_handler: logging.Handler = SlackHandler(webhook_url)
+    #    slack_handler.setLevel(slack_loglevel)
+    #    slack_formatter: logging.Formatter = SlackFormatter(app_name)
+    #    slack_handler.setFormatter(slack_formatter)
+    #    slack_logger.addHandler(slack_handler)
+    #except KeyError:
+    #    slack_logger.warning("No Slack webhook -- not logging to slack")
 
     return slack_logger
